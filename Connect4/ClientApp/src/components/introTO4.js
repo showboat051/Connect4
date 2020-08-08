aaf27bedf07ba40bc69b9685411bfb370da81e33
showboat051 <rashad.davis87@yahoo.com>
2020-08-08 18:21:21 -0500

    Add project files.

---
+++ Connect4/ClientApp/src/components/introTO4.js
@@ -0,0 +1,14 @@
+﻿import React, { Component } from 'react';
+
+export class intro extends Component {
+    static displayName = intro.name;
+
+    render() {
+        return (
+            <div>
+                <h1 id="Title">Connect Four!</h1>
+            </div>
+            )
+    }
+
+}
